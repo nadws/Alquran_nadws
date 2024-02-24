48a75c623591f5895d0befc06178fd198948fcfd
nadws <nandw567@gmail.com>
2024-02-24 13:55:20 +0800

    fix

--- src/app/list-surah/[detail]/page.jsx
+++ src/app/list-surah/[detail]/page.jsx
@@ -5,7 +5,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useState, useEffect } from "react";
 import { GoBookmark, GoHeart } from "react-icons/go";
 import DaftarSurah from "@/components/surah/index";
-export default function detail({ params }) {
+export default function Detail({ params }) {
   const [data, setData] = useState(null);
   // const [dataAyat, setDataAyat] = useState(null);
   const [isLoading, setLoading] = useState(true);
@@ -19,15 +19,6 @@ export default function detail({ params }) {
         setLoading(false);
       });
   }, []);
-
-  // useEffect(() => {
-  //   fetch("https://equran.id/api/v2/surat")
-  //     .then((res) => res.json())
-  //     .then((dataAyat) => {
-  //       setDataAyat(dataAyat);
-  //       setLoadingAyat(false);
-  //     });
-  // }, []);
 
   if (isLoading)
     return (
@@ -44,37 +35,6 @@ export default function detail({ params }) {
   return (
     <div className="flex flex-row">
       <div className="w-[30%] hidden lg:block  mr-5">
-        {/* <ScrollArea className="h-screen w-full ">
-          <div className=" grid grid-cols-1 gap-3">
-            {dataAyat &&
-              dataAyat.data &&
-              dataAyat.data.map((item) => (
-                <div
-                  className={`cursor-pointer rounded-lg bg-white mb-5 lg:mb-0 ${
-                    params.detail == item.nomor ? "active" : ""
-                  }`}
-                  key={item.nomor}
-                >
-                  <Link href={`/list-surah/${encodeURIComponent(item.nomor)}`}>
-                    <div className="h-36 p-6">
-                      <div className="flex justify-between items-center">
-                        <div className="text-1xl rounded-full bg-cyan-100 px-3 py-1">
-                          {item.nomor}
-                        </div>
-                        <div>
-                          <GoHeart className="text-4xl" />
-                        </div>
-                      </div>
-                      <div className="py-6 ">
-                        <h6 className="font-bold">{item.namaLatin}</h6>
-                        <p>{item.arti}</p>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              ))}
-          </div>
-        </ScrollArea> */}
         <DaftarSurah params={params.detail} />
       </div>
       <ScrollArea className="h-screen lg:w-[70%] w-[100%]  ">
