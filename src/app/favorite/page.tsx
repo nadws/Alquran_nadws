9f15f9970dd50aae060f3742cbdf05889a8c3a50
nadws <nandw567@gmail.com>
2024-02-24 23:39:22 +0800

    favorite added

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -1,4 +1,8 @@
 "use client";
 export default function Favorite() {
-  return <div>Favorite</div>;
+  return (
+    <div className="h-screen flex justify-center ">
+      <p className="text-6xl font-bold text-center">Belum Beolah ka</p>
+    </div>
+  );
 }
