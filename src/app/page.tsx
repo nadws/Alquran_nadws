cebddccd32e55706d075d620584ff3f88f27b157
nadws <nandw567@gmail.com>
2024-06-01 10:03:59 +0800

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="flex w-full justify-center items-center h-96 p-6">
       <div>
-        <h1>Halaman Awal Coming soon ...</h1>
+        <h5 className="text-base lg:text-3xl text-center mt-24 lg:mt-0">
+          Abdullah ibn Mas‘ud, Rasulullah shallallahu ‘alaihi wasallam bersabda,
+          “Siapa saja membaca satu huruf dari Kitabullah (Al-Qur’an), maka dia
+          akan mendapat satu kebaikan. Sedangkan satu kebaikan dilipatkan kepada
+          sepuluh semisalnya. Aku tidak mengatakan alif lâm mîm satu huruf. Akan
+          tetapi, alif satu huruf, lâm satu huruf, dan mîm satu huruf,” (HR.
+          At-Tirmidzi).
+        </h5>
       </div>
     </div>
   );
