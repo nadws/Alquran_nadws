22a5b9e94089caf17ff4b48c1015d671b3ec8dd3
nadws <nandw567@gmail.com>
2024-12-07 16:05:22 +0800

    update page.jsx

--- src/app/doa/page.jsx
+++ src/app/doa/page.jsx
@@ -13,62 +13,80 @@ import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { FiPlay } from "react-icons/fi";
 import { Input } from "@/components/ui/input";
 
-const Doa = () => {
-  const [filteredData, setFilteredData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+const Doa = ({ params }) => {
+  const [data, setData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://doa-doa-api-ahmadramadhan.fly.dev/api"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setFilteredData(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
+    fetch("https://doa-doa-api-ahmadramadhan.fly.dev/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
         setLoading(false);
-      }
-    };
-
-    fetchData();
+      });
   }, []);
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredData = data
+    ? data.filter((item) => {
+        const searchTermRegex = new RegExp(
+          searchTerm.toLowerCase().split("").join(".*")
+        );
+        return searchTermRegex.test(item.doa.toLowerCase());
+      })
+    : [];
+  console.log(filteredData);
+  if (isLoading) return <p>Loading ...</p>; // Mengatasi kasus ketika data masih dimuat
+
+  if (!data) return <></>; // Mengatasi kasus ketika data belum tersedia
+
   return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      {!loading && !error && filteredData.length === 0 && (
-        <p>Data tidak ditemukan</p>
-      )}
-      {!loading && !error && filteredData.length > 0 && (
-        <div>
-          {filteredData.map((item, index) => (
-            <div
-              key={index}
-              className="grid grid-rows-1 mb-2 bg-background border border-border p-8 rounded-lg"
-            >
-              <div className="flex justify-between items-center mb-10">
-                {item.doa}
+    <div className="grid grid-cols-1  ml-4 ">
+      <div className=" mt-4 mb-4 flex items-center justify-center">
+        <Input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearch}
+          placeholder="Search Doa..."
+          className="h-10 border border-slate-400 w-[90%] md:w-[70%] lg:w-[60%]"
+        />
+      </div>
+      <div>
+        <ScrollArea className="h-screen  mb-16 ">
+          {filteredData.map(
+            (
+              item,
+              index // Menggunakan filteredData yang sudah difilter
+            ) => (
+              <div
+                key={item.id}
+                className="grid grid-rows-1 mb-2 bg-background border border-border p-8 rounded-lg"
+              >
+                <div className="flex justify-between items-center mb-10">
+                  {item.doa}
+                </div>
+                <div>
+                  <div>
+                    <p className="text-end mb-6 leading-loose lg:leading-loose text-xl lg:text-2xl text-gray-500 dark:text-white font-lpmq">
+                      {item.ayat}
+                    </p>
+                    <p className="text-start text-sm lg:text-lg mb-2 text-[#38a482]">
+                      {item.latin}
+                    </p>
+                    <p className="text-start text-sm lg:text-lg ">
+                      {item.artinya}
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <p className="text-end mb-6 leading-loose lg:leading-loose text-xl lg:text-2xl text-gray-500 dark:text-white font-lpmq">
-                  {item.ayat}
-                </p>
-                <p className="text-start text-sm lg:text-lg mb-2 text-[#38a482]">
-                  {item.latin}
-                </p>
-                <p className="text-start text-sm lg:text-lg ">{item.artinya}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
+            )
+          )}
+        </ScrollArea>
+      </div>
     </div>
   );
 };
